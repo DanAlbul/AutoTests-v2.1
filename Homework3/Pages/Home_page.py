22f78d6c1ac625df711d4e159d9fecd636b75317
Daniel <51926545+DanAlbul@users.noreply.github.com>
2019-06-23 22:02:17 +0300

    Add files via upload

--- Homework3/Pages/Home_page.py
+++ Homework3/Pages/Home_page.py
@@ -1,48 +1,43 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 
-
 class HomePage(object):
-    def __init__(self, driver, base_url='https://anotepad.com/'):
-        self.base_url = base_url
-        self.driver = driver
+    def __init__(self, browser):
+        self.browser = browser
 
     def go_to_home_page(self):
-        self.driver.find_element_by_link_text("Home").click()
+        self.browser.find_element_by_link_text("Home").click()
 
-    def select_note(self, title):
-        self.driver.find_element_by_link_text(title).click()
+    def select_note(self, note):
+        self.browser.find_element_by_link_text(note).click()
 
     def public_or_private_setting(self):
-        try:
-            self.driver.find_element_by_xpath("//strong[contains(text(),'Public')]")
-        finally:
-            self.driver.find_element_by_xpath("//strong[contains(text(),'Private')]")
+        self.browser.find_element_by_css_selector(".caret").click()
 
     def public_checkbox(self):
-        select = Select.self.driver.find_element_by_id("accesspublic")
-        select.select_by_visible_text("Public Note")
-        self.driver.find_element_by_css_selector(".btn.btn-default").click()
+        self.browser.find_element_by_id("accesspublic").click()
+        self.browser.find_element_by_css_selector(".btn.btn-default").click()
 
     def private_checkbox(self):
-        select = Select.self.driver.find_element_by_id("accessprivate")
-        select.select_by_visible_text("Private Note")
-        self.driver.find_element_by_css_selector(".btn.btn-default").click()
+        self.browser.find_element_by_id("accessprivate").click()
+        self.browser.find_element_by_css_selector(".btn.btn-default").click()
 
     def public_selected_assert(self):
         try:
-            self.driver.find_element_by_xpath("//strong[contains(text(),'Public')]")
-        finally:
-            print("Note is public now!")
-            self.driver.quit()
+            WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
+                (By.XPATH, "//strong[contains(text(), 'Public Note')]")))
+           # self.browser.find_element_by_id("saveNoteMessage")
+        except NoSuchElementException:
+            print("Note private settings is not changed! Note is still public.")
+            return False
 
     def private_selected_assert(self):
         try:
-            self.driver.find_element_by_xpath("//strong[contains(text(),'Private')]")
-        finally:
-            print("Note is private now!")
-            self.driver.quit()
+            WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
+                (By.XPATH, "//strong[contains(text(), 'Private Note')]")))
+           # self.browser.find_element_by_id("saveNoteMessage")
+        except NoSuchElementException:
+            print("Note private settings is not changed! Note is still private.")
+            return False
--- Homework3/Pages/Settings_page.py
+++ Homework3/Pages/Settings_page.py
@@ -1,42 +1,39 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class Settings(object):
-
-    def __init__(self, driver, base_url='https://anotepad.com/'):
-        self.base_url = base_url
-        self.driver = driver
+    def __init__(self, browser):
+        self.browser = browser
 
     def go_to_settings_page(self):
-        self.driver.find_element_by_link_text("Settings").click()
+        self.browser.find_element_by_link_text("Settings").click()
 
     def change_email(self, email):
-        self.driver.find_element_by_css_selector("#email").send_keys(email)
-        self.driver.find_element_by_css_selector("[value='Update Email']").click()
+        self.browser.find_element_by_css_selector("#email").send_keys(email)
+        self.browser.find_element_by_css_selector("[value='Update Email']").click()
 
     def email_update_assertion(self):
         try:
-            WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(
+            WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
                 (By.XPATH, "//p[text()='Email updated successfully']")))
-        finally:
+        except NoSuchElementException:
             print("Email is not changed!")
-            self.driver.quit()
+            return False
 
     def change_password(self, password):
-        self.driver.find_element_by_css_selector("#password").send_keys(password)
-        self.driver.find_element_by_css_selector("#password_confirm").send_keys(password)
-        self.driver.find_element_by_css_selector("[value='Update Password']").click()
+        self.browser.find_element_by_css_selector("#password").send_keys(password)
+        self.browser.find_element_by_css_selector("#password_confirm").send_keys(password)
+        self.browser.find_element_by_css_selector("[value='Update Password']").click()
 
     def password_update_assertion(self):
         try:
-            WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(
+            WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
                 (By.XPATH, "//p[text()='Password successfully reset']")))
-        finally:
+        except NoSuchElementException:
             print("Password is not changed!")
-            self.driver.quit()
+            return False
 
     def update_any_settings(self):
-        self.driver.find_element_by_css_selector("[value='Update Settings']").click()
+        self.browser.find_element_by_css_selector("[value='Update Settings']").click()
